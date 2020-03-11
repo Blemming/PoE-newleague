81a3435da3118f652ab3d5134b1b25c41092cda1
Blemming <jveuxpotulsache@gmail.com>
2020-03-10 20:14:34 -0400

    started website

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+	<main style="background-image:url('https://api.poe.house/uploads/15381705a2404b3e8464c54b4ceb38f5.jpg')" class="relative flex  bg-cover bg-fixed overflow-hidden">
+		<section class="w-full flex flex-col items-center pt-16 bg-cover bg-fixed">
+			<section class="relative w-full lg:w-5/6 border-poe mb-4">
+				<div class="bg-poe-x text-yellow-poe-dark ">
+					<nuxt />
+				</div>
+			</section>
+		</section>
+	</main>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,56 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+	theme: {
+		fontFamily: {
+			serif: [
+				'Fontin',
+				'Georgia',
+				'Cambria',
+				'"Times New Roman"',
+				'Times',
+				'serif'
+			],
+			body: 'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
+		},
+		extend: {
+			boxShadow: {
+				black: '1px 1px 1px #000'
+			},
+			colors: {
+				black: {
+					50: 'rgba(0,0,0,0.5)',
+					85: 'rgba(0,0,0,0.85)',
+					100: 'rgba(0,0,0,1)'
+				},
+				'yellow-poe-light': '#cec59f',
+				'yellow-poe-dark': '#B49E79',
+				gray: {
+					100: '#f8f9fa',
+					200: '#e9ecef',
+					300: '#dee2e6',
+					400: '#ced4da',
+					500: '#adb5bd',
+					600: '#6c757d',
+					700: '#495057',
+					800: '#343a40;',
+					900: '#212529',
+					'poe-dark': '#050505'
+				}
+			},
+			spacing: {
+				80: '20rem',
+				96: '24rem'
+			}
+		}
+	},
+	variants: {
+		borderColor: ['responsive', 'hover', 'focus', 'focus-within'],
+		opacity: ['responsive', 'hover', 'focus', 'group-hover']
+	},
+	plugins: []
+};
